d1f6115dd67fded72affe608c4f5afb9acff0906
aphroditekenny <ragil.10124707@mahasiswa.unikom.ac.id>
2026-08-08 17:38:14 +0700

    fix: override app storagePath to /tmp/storage in api/index.php

--- mahasolve/api/index.php
+++ mahasolve/api/index.php
@@ -22,4 +22,19 @@ if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
 }
 
 // Forward Vercel requests to Laravel entrypoint
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
+    $app->useStoragePath('/tmp/storage');
+}
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
