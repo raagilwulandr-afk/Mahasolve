688e2b4c376006ddcef175bf0e1ac12adce40b26
aphroditekenny <ragil.10124707@mahasiswa.unikom.ac.id>
2026-08-08 19:26:24 +0700

    feat: complete end-to-end RPL Mahasolve implementation

--- mahasolve/api/index.php
+++ mahasolve/api/index.php
@@ -19,28 +19,12 @@ if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
     putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
     $_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
     $_SERVER['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
-
-    putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
-    $_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
-
-    putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
-    $_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
-
-    putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
-    $_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
-
-    putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
-    $_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
-
-    putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
-    $_ENV['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
 }
 
 // Forward Vercel requests to Laravel entrypoint
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-
 if (isset($_ENV['VERCEL']) || isset($_SERVER['VERCEL'])) {
     $app->useStoragePath('/tmp/storage');
 }
@@ -54,4 +38,3 @@ $response = $kernel->handle(
 $response->send();
 
 $kernel->terminate($request, $response);
-
